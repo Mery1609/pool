6e4b5bb3ca729f5e918801b2ba7964845315af48
agaly <a.jumaklychev@gmail.com>
2018-11-21 21:17:36 -0800

    my first program

---
+++ 1-My First Program/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("My name is AJ");
+		System.out.print("How are you?");
+		System.out.println("I am good/Thanks");
+		System.out.println("lol you");
+		System.out.print("yoookk");
+		int numberOfAnimals;
+		numberOfAnimals=100;
+		String zooName="the best zoo";
+		
+		
+		
+
+	}
+
+}
